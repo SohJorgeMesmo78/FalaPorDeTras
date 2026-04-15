6f558d3c22ef3272299a63ff70575796fd2db4c4
SohJorgeMesmo78 <jf78pereira@gmail.com>
2026-04-15 15:44:50 -0300

    feat: configure server-side prerendering routes for game pages and wildcard path

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'game/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => [
+      { id: 'quem-sou-eu' },
+      { id: 'cha-ou-cafe' }
+    ]
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
